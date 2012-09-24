2e96365be6dda4fbf753b64ff9d7b16ab49a9c8b
Bellfalasch <bellfalasch@gmail.com>
2012-09-24 16:08:34 +0200

    Some more settings on the get nav menu, and indented the title-tag better

--- WP-template/header.php
+++ WP-template/header.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="content-type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<?php if (is_search()) { ?>
 	   <meta name="robots" content="noindex, nofollow" /> 
 	<?php } ?>
 	<title><?php
-		      if (function_exists('is_tag') && is_tag()) {
-		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
-		      elseif (is_archive()) {
-		         wp_title(''); echo ' Archive - '; }
-		      elseif (is_search()) {
-		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-		      elseif (!(is_404()) && (is_single()) || (is_page())) {
-		         wp_title(''); echo ' - '; }
-		      elseif (is_404()) {
-		         echo 'Not Found - '; }
-		      if (is_home()) {
-		         bloginfo('name'); echo ' - '; bloginfo('description'); }
-		      else {
-		          bloginfo('name'); }
-		      if ($paged>1) {
-		         echo ' - page '. $paged; }
-		   ?></title>
+			if (function_exists('is_tag') && is_tag()) {
+				single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+			elseif (is_archive()) {
+				wp_title(''); echo ' Archive - '; }
+			elseif (is_search()) {
+				echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+			elseif (!(is_404()) && (is_single()) || (is_page())) {
+				wp_title(''); echo ' - '; }
+			elseif (is_404()) {
+				echo 'Not Found - '; }
+			if (is_home()) {
+				bloginfo('name'); echo ' - '; bloginfo('description'); }
+			else {
+				 bloginfo('name'); }
+			if ($paged>1) {
+				echo ' - page '. $paged; }
+			?></title>
 	<link rel="shortcut icon" href="/favicon.ico">
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	<?php wp_head(); ?>
@@ -40,7 +41,9 @@
 		<div id="content-wrap">
 			<?php wp_nav_menu(array(
 				'menu' => 'Huvudmeny',
-				'menu_id' => 'nav'
+				'menu_id' => 'nav',
+				'container' => false,
+				'depth' => 1
 			)); ?>
 			
 			<div id="content">
